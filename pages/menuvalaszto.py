f191cc225461c2557a787c3f08c47e009637bde3
szellkalmanter <darabo.srichard20@gmail.com>
2026-05-27 20:31:40 +0200

    Update message on menu selection page

--- pages/menuvalaszto.py
+++ pages/menuvalaszto.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎛️ Menüválasztó Oldal")
-st.write("Sikeresen átléptél a másik URL-re!")
+st.write("Vállalati hitel nyilvántartás")
 
 # Visszalépés gomb a főoldalra (URL-váltással)
 if st.button("⬅️ Vissza a Belépőre"):
