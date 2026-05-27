096e5b285d59eeb8bea876f409056e9ce55e0d06
szellkalmanter <darabo.srichard20@gmail.com>
2026-05-27 15:42:20 +0200

    Update login.py

--- login.py
+++ login.py
@@ -65,3 +65,18 @@ if not st.session_state['bejelentkezve']:
 
         except FileNotFoundError:
             st.info("Még nem történt bejelentkezés (a napló üres).")
+
+import streamlit as st
+import pandas as pd
+from faker import Faker
+import random
+st.set_page_config(
+    page_title="Menüválasztó", 
+    layout="centered"
+st.title("Menüválasztó")
+st.divider()
+st.page_link("vallalatihitel.py", label="Vállalati Hitelezés",)
+st.divider()
+st.page_link("Faktorálás.py", label=("Faktorálás",)
+st.divider()
+st.page_link("login.py", label=("Kilépés",)
