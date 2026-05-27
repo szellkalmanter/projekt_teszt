eb683b9ed4e735c7ad2e66198d83922803d9b909
szellkalmanter <darabo.srichard20@gmail.com>
2026-05-27 20:29:19 +0200

    Update menuvalaszto.py

--- pages/menuvalaszto.py
+++ pages/menuvalaszto.py
@@ -1,14 +1,8 @@
 import streamlit as st
-import pandas as pd
-from faker import Faker
-import random
-st.set_page_config(
-    page_title="Menüválasztó", 
-    layout="centered"
-st.title("Menüválasztó")
-st.divider()
-st.page_link("vallalatihitel.py", label="Vállalati Hitelezés",)
-st.divider()
-st.page_link("Faktorálás.py", label=("Faktorálás",)
-st.divider()
-st.page_link("login.py", label=("Kilépés",)
+
+st.title("🎛️ Menüválasztó Oldal")
+st.write("Sikeresen átléptél a másik URL-re!")
+
+# Visszalépés gomb a főoldalra (URL-váltással)
+if st.button("⬅️ Vissza a Belépőre"):
+    st.switch_page("login.py")
