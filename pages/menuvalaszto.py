ca9676186d01ac98b14d527a4ad22943b5c125fd
szellkalmanter <darabo.srichard20@gmail.com>
2026-05-27 20:32:23 +0200

    Update menuvalaszto.py

--- pages/menuvalaszto.py
+++ pages/menuvalaszto.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🎛️ Menüválasztó Oldal")
 st.write("Vállalati hitel nyilvántartás")
-
+st.write("Faktor nyilvántartás")
 # Visszalépés gomb a főoldalra (URL-váltással)
 if st.button("⬅️ Vissza a Belépőre"):
     st.switch_page("login.py")
